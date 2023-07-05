b1e5aae5153223360fd7843432b8fa750c98094e
Bijay Shrestha <108262068+Bijay-Shre-stha@users.noreply.github.com>
2023-07-05 18:10:18 +0545

    commited for deyploment

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -24,7 +24,7 @@ export default function Navbar(props) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
-                        <a className="nav-link" href="#">
+                        <a className="nav-link" href="/">
                             Home <span className="sr-only"></span>
                         </a>
                     </li>
